4a94850ae3bda1287eb746d74727a431ea2b4037
NeiltonSeguins <seguins.neilton@gmailcom>
2024-05-14 15:16:34 -0300

    Vídeo 5.2 - Realizando cache de arquivos estáticos

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import { registerSW } from "./registerSW.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
@@ -9,4 +8,3 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   </React.StrictMode>
 );
 
-registerSW();
